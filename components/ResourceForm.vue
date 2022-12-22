c90f6e9eb8672d7704242b5db7a896a462b38efe
Myzwer <josh@onefortyfivedesign.com>
2022-12-22 15:24:15 -0500

    :construction: Added a component for hte form, still building it out.

---
+++ components/ResourceForm.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<form class="">
+			<div class="grid grid-cols-12 gap-4 pt-10 pb-12">
+				<div class="col-span-12 md:col-span-6 text-left">
+					<input
+						id="grid-first-name"
+						class="appearance-none block w-full bg-white text-black border rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+						type="text"
+						placeholder="Jane">
+					<label
+						class="uppercase tracking-wide text-xs font-bold mb-1 ml-3"
+						for="grid-first-name">
+						First Name
+					</label>
+				</div>
+			</div>
+
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ResourceForm"
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,48 @@
 <template>
 	<div>
 		<div class="index-h1-background">
+			<div class="py-10">
+				<h1 class="text-center text-black text-9xl">Are You Spent?</h1>
 
+				<div class="text-center mx-auto max-w-2xl md:max-w-7xl">
+					<div class="bg-orange border-8 border-black">
+						<div class="grid grid-cols-12 gap-4 pt-10 pb-12">
+							<div class="col-span-12 md:col-span-5 px-5">
+								<h3 class="text-3xl body-font font-bold">Your Total Monthly Budget</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
+
+		<div class="bg-green">
+			<div class="text-center mx-auto max-w-2xl md:max-w-7xl">
+				<div class="grid grid-cols-12 gap-4 pt-10 pb-12">
+					<div class="col-span-12 px-5">
+						<div class="py-5 text-white">
+							<h2 class="text-center text-8xl">Free Resource</h2>
+							<p class="text-center font-bold">Download the pdf that has info and stuff</p>
+
+							<ResourceForm />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="grid grid-cols-12">
+			<div class="col-span-12 md:col-span-6 px-5 bg-yellow text-black">
+				<h2 class="text-center text-8xl">Free Resource</h2>
+				<p class="text-center font-bold">Download the pdf that has info and stuff</p>
+			</div>
+
+			<div class="col-span-12 md:col-span-6 px-5 bg-white text-black">
+				<h2 class="text-center text-8xl">Free Resource</h2>
+				<p class="text-center font-bold">Download the pdf that has info and stuff</p>
+			</div>
+		</div>
+
 	</div>
 </template>
 
@@ -15,16 +55,15 @@ export default {
 <style>
 /* stylelint-disable color-function-notation */
 
-/* .index-h1-background {
+.index-h1-background {
 	background: linear-gradient(
-		rgba(0, 0, 0, 70%),
-		rgba(0, 0, 0, 70%)
-	), url("~static/images/index/index-head-bg.jpg");
-	height: 60vh;
+		rgba(0, 0, 0, 0%),
+		rgba(0, 0, 0, 0%)
+	), url("~static/images/graph.jpg");
 	background-position: center center;
 	background-repeat: no-repeat;
 	background-size: cover;
-} */
+}
 
 /* stylelint-enable color-function-notation */
 </style>
