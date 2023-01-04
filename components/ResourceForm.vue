87fe8805e8caeb839ddb6c3c993c6cf883ddb3bf
Myzwer <josh@onefortyfivedesign.com>
2023-01-04 15:14:38 -0500

    :art: Finished the styling on the form.

--- components/ResourceForm.vue
+++ components/ResourceForm.vue
@@ -2,17 +2,46 @@
 	<div>
 		<form class="">
 			<div class="grid grid-cols-12 gap-4 pt-10 pb-12">
-				<div class="col-span-12 md:col-span-6 text-left">
+				<div class="col-span-12 md:col-span-6 text-left mb-6">
 					<input
 						id="grid-first-name"
 						class="appearance-none block w-full bg-white text-black border rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
 						type="text"
-						placeholder="Jane">
+						placeholder="John">
 					<label
-						class="uppercase tracking-wide text-xs font-bold mb-1 ml-3"
+						class="uppercase tracking-wide text-xs font-bold ml-1"
 						for="grid-first-name">
 						First Name
 					</label>
+				</div>
+				<div class="col-span-12 md:col-span-6 text-left">
+					<input
+						id="grid-last-name"
+						class="appearance-none block w-full bg-white text-black border rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+						type="text"
+						placeholder="Doe">
+					<label
+						class="uppercase tracking-wide text-xs font-bold ml-1"
+						for="grid-last-name">
+						Last Name
+					</label>
+				</div>
+				<div class="col-span-12 text-left">
+					<input
+						id="grid-email"
+						class="appearance-none block w-full bg-white text-black border rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+						type="text"
+						placeholder="johndoe@gmail.com">
+					<label
+						class="uppercase tracking-wide text-xs font-bold ml-1"
+						for="grid-email">
+						Email
+					</label>
+				</div>
+				<div class="col-span-12 text-left">
+					<button class="bg-orange hover:bg-red shadow-lg text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+						Submit
+					</button>
 				</div>
 			</div>
 
